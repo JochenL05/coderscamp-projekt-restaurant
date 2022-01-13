b30e44e1daf099da39a62702b077255042c70efd
Radoslaw Janecki <radek@janecki.info>
2022-01-13 22:07:01 +0100

    initial setup

---
+++ src/views/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topbar from '../../components/Topbar/Topbar'
+
+const Homepage = () => {
+    return (
+        <div>
+            <Topbar/>
+        </div>
+    )
+}
+
+export default Homepage
