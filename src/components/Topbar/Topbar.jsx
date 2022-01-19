dc13e7f95835dd4c2e7f8efb5870f682946aa4ee
Radoslaw Janecki <radek@janecki.info>
2022-01-19 18:20:14 +0100

    Working Topbar, first draft

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Topbar.css";
 
-const Topbar = () => {
-    return <div className="topbar">Topbar</div>;
+function Topbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            Restauracja
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
+                Strona Główna
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/about" activeClassName="active" className="nav-links" onClick={handleClick}>
+                O nas
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/ContactPage" activeClassName="active" className="nav-links" onClick={handleClick}>
+                Kontakt
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
-
-export default Topbar
+export default Topbar;
--- src/views/Homepage/Homepage.jsx
+++ src/views/Homepage/Homepage.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Topbar from "../../components/Topbar/Topbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div>
-      <Topbar />
+      <Router>
+        <Topbar />
+      </Router>
       <Footer />
     </div>
   );
 };
 
-export default Homepage
+export default Homepage;
