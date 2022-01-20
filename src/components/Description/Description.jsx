f0d0471208e1b976c64a8da27f683f58f1f11596
JochenL05 <jochenlippok@gmail.com>
2022-01-20 23:09:35 +0100

    Adding mainview and editing description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,9 +3,9 @@ import "./Description.css";
 
 const Description = () => {
   return (
-    <div>
-      <h1 className="primary">Restaurant name</h1>
-      <h2 className="secondary">Description</h2>
+    <div className="all">
+      <h1 className="primary">About our restaurant</h1>
+      <p className="secondary">Description</p>
     </div>
   );
 };
---
+++ src/components/Mainview/Mainview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Mainview.css";
+
+const mainview = () => {
+  return (
+    <div className="main">
+      <h1 className="opener">Welcome!</h1>
+      <button className="click">Reservation</button>
+    </div>
+  );
+};
+
+export default mainview;
