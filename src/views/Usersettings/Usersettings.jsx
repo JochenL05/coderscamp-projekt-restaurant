4e139c63a037cbfc9d28cd747042382a41f59a38
JochenL05 <jochenlippok@gmail.com>
2022-01-21 17:37:15 +0100

    Creating a new branch

---
+++ src/views/Usersettings/Usersettings.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Topbar from "../../components/Topbar/Topbar";
+
+const Usersettings = () => {
+  return (
+    <div>
+      <Topbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Usersettings;
