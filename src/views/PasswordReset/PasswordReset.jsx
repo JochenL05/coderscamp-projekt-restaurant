44e395c58682ae6ddb182fefe32de9d2a26dde08
justynas <staniszewskajustyn@gmail.com>
2022-01-29 16:32:02 +0100

    style changes

--- src/views/PasswordReset/PasswordReset.jsx
+++ src/views/PasswordReset/PasswordReset.jsx
@@ -11,12 +11,16 @@ const PasswordReset = () => {
         <form className="password-reset-form">
           <h1>Reset your password</h1>
           <div className="form-inputs">
-            <label className="form-label">Password</label>
-            <input type="password" className="form-input" placeholder="Enter your new password"></input>
+            <label for="password" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-input" placeholder="Enter your new password" id="password"></input>
           </div>
           <div className="form-inputs">
-            <label className="form-label">Password Confirm</label>
-            <input type="password" className="form-input" placeholder="Confirm your password"></input>
+            <label for="passwordConfirm" className="form-label">
+              Password Confirm
+            </label>
+            <input type="password" className="form-input" placeholder="Confirm your password" id="passwordConfirm"></input>
           </div>
           <button className="password-reset-btn" type="submit">
             Reset Password
