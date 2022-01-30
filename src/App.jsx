3b5f4c82c5c25d0b9162f8f3353a13d19bc66b6a
Radoslaw Janecki <radek@janecki.info>
2022-01-30 23:12:01 +0100

    Added global routing to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import Topbar from "./components/Topbar/Topbar";
+import Footer from "./components/Footer/Footer";
 import Homepage from "./views/Homepage/Homepage";
+// import Menu from "./views/Menu/Menu";
+// import About from "./views/About/About";
+// import Reservation from "./views/Reservation/Reservation";
+// import ContactPage from "./views/ContactPage/ContactPage";
+import { Routes, Route } from "react-router-dom";
 import React from "react";
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Topbar />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        {/* <Route path="/menu" element={<Menu />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/reservation" element={<Reservation />} /> */}
+        {/* <Route path="/contact-page" element={<ContactPage />} /> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/views/Homepage/Homepage.jsx
+++ src/views/Homepage/Homepage.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import Footer from "../../components/Footer/Footer";
-import Topbar from "../../components/Topbar/Topbar";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const Homepage = () => {
-  return (
-    <div>
-      <Router>
-        <Topbar />
-      </Router>
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Homepage;
