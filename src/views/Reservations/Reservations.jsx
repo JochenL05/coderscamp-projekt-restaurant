5b33e389260d49fe4bbfd063994bef571a8e8b97
justynas <staniszewskajustyn@gmail.com>
2022-01-30 14:02:52 +0100

    prettier

--- src/views/Reservations/Reservations.jsx
+++ src/views/Reservations/Reservations.jsx
@@ -12,23 +12,48 @@ const Reservations = () => {
           <h1>Make a reservation</h1>
           <div className="form-inputs">
             <label className="form-label">* Name</label>
-            <input type="text" name="name" className="form-input" placeholder="Name" />
+            <input
+              type="text"
+              name="name"
+              className="form-input"
+              placeholder="Name"
+            />
           </div>
           <div className="form-inputs">
             <label className="form-label">* Phone</label>
-            <input type="tel" name="phone" className="form-input" placeholder="Phone" />
+            <input
+              type="tel"
+              name="phone"
+              className="form-input"
+              placeholder="Phone"
+            />
           </div>
           <div className="form-inputs">
             <label className="form-label">* Email</label>
-            <input type="email" name="email" className="form-input" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              className="form-input"
+              placeholder="Email"
+            />
           </div>
           <div className="form-inputs">
             <label className="form-label">* Date</label>
-            <input type="date" name="date" className="form-input" placeholder="Date" />
+            <input
+              type="date"
+              name="date"
+              className="form-input"
+              placeholder="Date"
+            />
           </div>
           <div className="form-inputs">
             <label className="form-label">* Time</label>
-            <input type="time" name="time" className="form-input" placeholder="Time" />
+            <input
+              type="time"
+              name="time"
+              className="form-input"
+              placeholder="Time"
+            />
           </div>
           <div className="form-inputs">
             <label className="form-label">
@@ -43,7 +68,12 @@ const Reservations = () => {
           </div>
           <div className="form-inputs">
             <label className="form-label">Note</label>
-            <input type="text" name="note" className="form-input" placeholder="Note" />
+            <input
+              type="text"
+              name="note"
+              className="form-input"
+              placeholder="Note"
+            />
           </div>
           <button className="form-input-btn" type="submit">
             BOOK TABLE
