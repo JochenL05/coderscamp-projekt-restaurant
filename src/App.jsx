edda1d3401c58a3b3b1db4c3fc62d792f88c2265
JochenL05 <jochenlippok@gmail.com>
2022-02-01 15:42:27 +0100

    Little change

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
+import React from "react";
 import "./App.css";
-import Footer from "./views/Footer/Footer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./views/Homepage/Homepage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Footer />
-        <div className="content">
-          <Switch>
-            <Route path="/">
-              <Homepage />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <Homepage />
+    </div>
   );
 }
 
