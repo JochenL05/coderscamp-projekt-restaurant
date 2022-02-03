afcfc7c861414712fd407e3c89f47c60176d7394
JochenL05 <jochenlippok@gmail.com>
2022-02-03 19:10:29 +0100

    Creating new branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 //import Homepage from "./views/Homepage/Homepage";
-import Usersettings from "./views/Usersettings/Usersettings";
-//import Oldorder from "./views/Oldorder/Oldorder";
+import Oldorder from "./views/Oldorder/Oldorder";
 
 function App() {
   return (
     <div className="App">
-      <Usersettings />
+      <Oldorder />
     </div>
   );
 }
