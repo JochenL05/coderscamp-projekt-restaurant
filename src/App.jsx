00ad0e381025618117c7b0145914dad86ee105f0
Iwona Gorbacz <i.gorbacz@gmail.com>
2022-02-04 15:07:46 +0100

    fixed problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-//import Homepage from './views/Homepage/Homepage';
+import Homepage from "./views/Homepage/Homepage";
 //import ContactPage from "./views/ContactPage/ContactPage";
 import React from "react";
-import LoginPage from "./views/LoginPage/LoginPage";
+//import LoginPage from "./views/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Homepage />
     </div>
   );
 }
