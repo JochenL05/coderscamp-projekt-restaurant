8fba1e3d2ffb1f39f0a1c02a2e08e71789cc9e91
JochenL05 <jochenlippok@gmail.com>
2022-02-05 17:51:47 +0100

    Add footer and topbar in OrderDetails

--- src/components/OldOrderDetails/OldOrderDetails.jsx
+++ src/components/OldOrderDetails/OldOrderDetails.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./OldOrderDetails.css";
 import OrderArr from "../../data/OrderArr";
+import Topbar from "../Topbar/Topbar";
+import Footer from "../Footer/Footer";
 
 const OldOrderDetails = () => {
   let { orderID } = useParams();
@@ -14,24 +16,28 @@ const OldOrderDetails = () => {
   }, []);
 
   return (
-    <div className="orderList">
-      <div className="orderList_header">Date</div>
-      <div className="orderList_header">Delivery Address</div>
-      <div className="orderList_header">Dishes Ordered</div>
-      <div className="orderList_header">Price</div>
-      <div className="orderList_data">{order[0].orderDate}</div>
-      <div className="orderList_data">{order[0].deliveryAddress}</div>
-      <div className="orderList_data">
-        {order[0].orderedDishes.map((dish, index) => {
-          return (
-            <p key={index}>
-              {dish.dishName} - {dish.dishPrice}
-            </p>
-          );
-        })}
+    <>
+      <Topbar />
+      <div className="orderList">
+        <div className="orderList_header">Date</div>
+        <div className="orderList_header">Delivery Address</div>
+        <div className="orderList_header">Dishes Ordered</div>
+        <div className="orderList_header">Price</div>
+        <div className="orderList_data">{order[0].orderDate}</div>
+        <div className="orderList_data">{order[0].deliveryAddress}</div>
+        <div className="orderList_data">
+          {order[0].orderedDishes.map((dish, index) => {
+            return (
+              <p key={index}>
+                {dish.dishName} - {dish.dishPrice}
+              </p>
+            );
+          })}
+        </div>
+        <div className="orderList_data">{order[0].orderPrice}</div>
       </div>
-      <div className="orderList_data">{order[0].orderPrice}</div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
