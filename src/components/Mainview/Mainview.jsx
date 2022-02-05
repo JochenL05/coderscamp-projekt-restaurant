793aeab4b30038da39f3fe64c9031ef7b575af09
JochenL05 <jochenlippok@gmail.com>
2022-02-05 12:43:45 +0100

    Added react router dom

--- src/components/Mainview/Mainview.jsx
+++ src/components/Mainview/Mainview.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Mainview.css";
+import { Link } from "react-router-dom";
 
 const Mainview = () => {
   return (
     <div className="main">
       <h1 className="main__title">Welcome!</h1>
-      <button className="main__button">Reservation</button>
+      <Link to={`/reservations`}>
+        <button className="main__button">Reservation</button>
+      </Link>
     </div>
   );
 };
